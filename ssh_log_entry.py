ee9842e36386db935ff8c0dd4ebf9cbee86a88fd
Svyatoslav Shypka <svyat200909@gmail.com>
2024-04-27 19:21:39 +0200

    1 done

--- ssh_log_entry.py
+++ ssh_log_entry.py
@@ -5,9 +5,9 @@ from ipaddress import IPv4Address
 class SSHLogEntry:
     def __init__(self, timestamp, message, pid=None, hostname=None):
         self.timestamp = timestamp
+        self.hostname = hostname
         self.message = message
         self.pid = pid
-        self.hostname = hostname
 
     def __str__(self):
         entry_str = f"Timestamp: {self.timestamp}, Message: {self.message}"
@@ -18,7 +18,7 @@ class SSHLogEntry:
         return entry_str
 
     def extract_ipv4_address(self):
-        ip_pattern = r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b"
+        ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
         ip_matches = re.findall(ip_pattern, self.message)
         if ip_matches:
             return IPv4Address(ip_matches[0])
