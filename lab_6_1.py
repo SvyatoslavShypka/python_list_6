b7afbc58921a3b436242c59c680e4b2b1c4dcbd3
Svyatoslav Shypka <svyat200909@gmail.com>
2024-04-28 20:21:37 +0200

    1 and 2 done 3-6 done all 7 done All finished

--- lab_6_1.py
+++ lab_6_1.py
@@ -4,7 +4,7 @@ from lab_5_1_3statistics import convert_str_to_datetime
 
 if __name__ == '__main__':
     lista_dict = read_log(None)
-    # TEST:    type SSH.log | python lab_6_1.py
+
     # test_log_entry.__init__
     test_log_entry = SSHLogEntry(convert_str_to_datetime(lista_dict[0].get("date")),
                             lista_dict[0].get("message"), lista_dict[0].get("PID"), lista_dict[0].get("hostname"))
@@ -14,3 +14,5 @@ if __name__ == '__main__':
     # Testujemy metodę extract_ipv4_address
     print(type(test_log_entry.extract_ipv4_address()))
     print(test_log_entry.extract_ipv4_address())
+
+    # TEST:    type SSH.log | python lab_6_1.py
